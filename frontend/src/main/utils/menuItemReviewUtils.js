8a56f417a4756277f774fa0264d23a1646208b52
Ethan Sun <ethansun210@gmail.com>
2023-08-14 16:56:10 -0700

    es - fix coverage of utils

--- frontend/src/main/utils/menuItemReviewUtils.js
+++ frontend/src/main/utils/menuItemReviewUtils.js
@@ -1,10 +1,5 @@
 import { toast } from "react-toastify";
 
-export function onDeleteSuccess(message) {
-    console.log(message);
-    toast(message);
-}
-
 export function cellToAxiosParamsDelete(cell) {
     return {
         url: "/api/menuitemreview",
