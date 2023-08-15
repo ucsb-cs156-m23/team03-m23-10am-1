2b4529fa5fcd5d442a1aa4aa56acd28188b2eeb9
EthanSun210 <ethansun210@gmail.com>
2023-08-15 03:42:37 -0700

    es - fix MenuItemReviewRepository

--- src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -1,11 +1,10 @@
 package edu.ucsb.cs156.example.entities;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,14 +15,16 @@ import lombok.Builder;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "menuitemreview")
+
+@Entity(name = "MenuItemReview")
 public class MenuItemReview {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-  private Long itemId;
-  private String email;
-  private Integer stars;
-  private LocalDateTime dateReviewed;
-  private String comments;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long itemId;
+    private String reviewerEmail;
+    private int stars;
+    private LocalDateTime dateReviewed;
+    private String comments;
+    
 }
--- src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuItemReviewRepository extends CrudRepository<MenuItemReview, Long> {
- 
+
 }
