ba6365c7feb9d4a1cffc34514500f4714d7e42ba
marcushuang1 <ziyang_huang@ucsb.edu>
2023-08-14 21:19:40 -0700

    organizationFixtures update

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,31 @@
+const ucsbOrganizationFixtures = {
+    oneOrganization: {
+        "orgCode": "KFC",
+        "orgTranslationShort": "Kentucky Fried Chi",
+        "orgTranslation": "Kentucky Fried Chicken",
+        "inactive": false
+    },
+    threeOrganizations: [
+        {
+            "orgCode": "KFC",
+            "orgTranslationShort": "Kentucky Fried Chi",
+            "orgTranslation": "Kentucky Fried Chicken",
+            "inactive": "false"
+        },
+        {
+            "orgCode": "ABC",
+            "orgTranslationShort": "Alpha Bang Cloud",
+            "orgTranslation": "Alpha Bang Cloud",
+            "inactive": "true"
+        },
+        {
+            "orgCode": "IOSS",
+            "orgTranslationShort": "One Stop",
+            "orgTranslation": "Import One Stop Shop",
+            "inactive": "true"
+        }
+    ]
+};
+
+
+export { ucsbOrganizationFixtures };
