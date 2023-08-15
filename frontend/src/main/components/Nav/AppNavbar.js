38239dd7c912c0e2eb4d918c0b8435f8577e84e6
marcushuang1 <ziyang_huang@ucsb.edu>
2023-08-14 22:59:43 -0700

    organizationapp updated

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -56,9 +56,9 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                 <>
                   <Nav.Link as={Link} to="/restaurants">Restaurants</Nav.Link>
                   <Nav.Link as={Link} to="/ucsbdates">UCSB Dates</Nav.Link>
+                  <Nav.Link as={Link} to="/ucsborganization">ucsbOrganization</Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">Placeholder</Nav.Link>
                   <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">UCSBDiningCommonsMenuItem</Nav.Link>
-                  <Nav.Link as={Link} to="/ucsborganization">ucsbOrganization</Nav.Link>
                 </>
               )
             }
